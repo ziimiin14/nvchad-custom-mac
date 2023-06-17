ac161878850dd32a679ee5ce583f4c1b6bbc9deb
ziimiin14 <erzimin@gmail.com>
2023-06-18 00:32:10 +0800

    Stylua format lua files

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -2,38 +2,38 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+	},
 }
 
 M.tabufline = {
-  n = {
-    ["<leader>X"] = {
-      function()
-        require("nvchad_ui.tabufline").closeAllBufs()
-      end,
-      "Close all buffers",
-    },
+	n = {
+		["<leader>X"] = {
+			function()
+				require("nvchad_ui.tabufline").closeAllBufs()
+			end,
+			"Close all buffers",
+		},
 
-    ["<leader>C"] = {
-      function()
-        require("nvchad_ui.tabufline").closeOtherBufs()
-      end,
-      "Close all buffers except current one",
-    },
-  },
+		["<leader>C"] = {
+			function()
+				require("nvchad_ui.tabufline").closeOtherBufs()
+			end,
+			"Close all buffers except current one",
+		},
+	},
 }
 
 M.telescope = {
-  n = {
-    -- find
-    ["<leader>fg"] = { "<cmd> Telescope git_files <CR>", "Find files tracked by Git" },
+	n = {
+		-- find
+		["<leader>fg"] = { "<cmd> Telescope git_files <CR>", "Find files tracked by Git" },
 
-    -- git
-    ["<leader>gc"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
-    ["<leader>gb"] = { "<cmd> Telescope git_branches <CR>", "Git branches" },
-  },
+		-- git
+		["<leader>gc"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
+		["<leader>gb"] = { "<cmd> Telescope git_branches <CR>", "Git branches" },
+	},
 }
 
 -- more keybinds!
